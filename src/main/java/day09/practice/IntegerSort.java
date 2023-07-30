2882ba7c1a4181e648fc015fa3341f2b095c1fda
prathiusha22 <116252266+prathiusha22@users.noreply.github.com>
2023-07-30 18:18:24 +0530

    day09practice commits

---
+++ src/main/java/day09/practice/IntegerSort.java
@@ -0,0 +1,43 @@
+package day09.practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class IntegerSort {
+	
+
+public static boolean addIntegerToArray(ArrayList<Integer> intList) throws IllegalArgumentException {
+		
+		if(intList == null) {
+			throw new IllegalArgumentException("ArrayList Can't be Null");
+		}
+		
+		intList.add(8);
+		intList.add(9);
+		intList.add(45);
+		intList.add(12);
+		intList.add(1);
+		
+		Collections.sort(intList);
+		
+		System.out.print("Output: ");
+		
+		for(int i=0; i<intList.size(); i++) {
+			System.out.print(intList.get(i) + " ");
+		}
+		
+		return true;
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		
+		addIntegerToArray(intList);
+
+	}
+}
